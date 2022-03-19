1d69c43a99349dbbfee89e67f87b173771da70d4
Jide Adesanya <Jide1012@yahoo.com>
2022-03-19 12:06:58 -0500

    worked on js file and html

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,62 @@
-// Assignment code here
+// Arrays of characters
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChars = "!@#$%^&*()";
+var numbers = "1234567890";
+var includeLower = confirm("Include lowercase?");
+var includeUpper = confirm("Include uppercase?");
+var includeSpecial = confirm("Include special characers?");
+var includeNumber = confirm("Include numbers?");
 
+var numberOfChars = prompt("How many characters?");
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var password = "";
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function getCombinedArrays() {
+  var finalCharacterArray = [];
 
-  passwordText.value = password;
+  if (includeLower) {
+    finalCharacterArray = finalCharacterArray.concat(
+      lowerCaseLetters.split("")
+    );
+  }
 
+  if (includeUpper) {
+    finalCharacterArray = finalCharacterArray.concat(
+      upperCaseLetters.split("")
+    );
+  }
+
+  if (includeSpecial) {
+    finalCharacterArray = finalCharacterArray.concat(specialChars.split(""));
+  }
+
+  if (includeNumber) {
+    finalCharacterArray = finalCharacterArray.concat(numbers.split(""));
+  }
+
+  return finalCharacterArray;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function init() {
+  var arrayOfChars = getCombinedArrays();
+
+  console.log("FINAL ARRAY OF CHARS:", arrayOfChars);
+  var intNumberOfChars = parseInt(numberOfChars);
+
+  for (var i = 0; i < intNumberOfChars; i++) {
+    var randomIndex = Math.floor(Math.random() * arrayOfChars.length);
+
+    password = password + arrayOfChars[randomIndex];
+  }
+
+  generatevar.addEventListener ('click', () => {
+    const length=length.value
+    
+  } )
+
+
+  console.log("RANDOM PASSWORD IS: ", password);
+}
+
+init();
